9d61ffb1c1ab7129badceff225e7ace2cf846ace
Joakim Jönsson <jockejockejockejocke@gmail.com>
2023-01-13 13:04:25 +0100

    Added filter function.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,21 @@ import Todo from './components/Todo';
 import Form from './components/Form';
 import FilterButton from './components/FilterButton';
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed,
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState('All');
+
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton key={name} name={name} isPressed={name === filter} setFilter={setFilter} />
+  ));
 
   function toggleTaskCompleted(id) {
     const updatedTasks = tasks.map((task) => {
@@ -16,20 +29,21 @@ function App(props) {
       return task;
     });
     setTasks(updatedTasks);
-    console.log(updatedTasks);
   }
 
-  const taskList = tasks.map((task) => (
-    <Todo
-      key={task.id}
-      id={task.id}
-      name={task.name}
-      completed={task.completed}
-      toggleTaskCompleted={toggleTaskCompleted}
-      deleteTask={deleteTask}
-      editTask={editTask}
-    />
-  ));
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map((task) => (
+      <Todo
+        key={task.id}
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
+    ));
 
   function addTask(name) {
     const newTask = { id: `todo-${nanoid()}`, name, completed: false };
@@ -58,11 +72,7 @@ function App(props) {
     <div className='todoapp stack-large'>
       <h1>React Todo App</h1>
       <Form addTask={addTask} />
-      <div className='filters btn-group stack-exception'>
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
-      </div>
+      <div className='filters btn-group stack-exception'>{filterList}</div>
       <h2 id='list-heading'>{headingText}</h2>
       <ul role='list' className='todo-list stack-large stack-exception' aria-labelledby='list-heading'>
         {taskList}
