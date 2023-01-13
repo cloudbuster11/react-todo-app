61504593c8bb5651d0a96aa89fa2856032f0184c
Joakim Jönsson <jockejockejockejocke@gmail.com>
2023-01-13 11:21:55 +0100

    Removed typo.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
 
   function toggleTaskCompleted(id) {
-  g  const updatedTasks = tasks.map((task) => {
+    const updatedTasks = tasks.map((task) => {
       if (id === task.id) {
         return { ...task, completed: !task.completed };
       }
